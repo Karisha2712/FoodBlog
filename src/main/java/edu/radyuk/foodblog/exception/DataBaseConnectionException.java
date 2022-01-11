95357b97332663e333ab00980f4b5a6f5cbb872a
Karina Radyuk <radyuk.karina@bk.ru>
2022-01-11 17:40:46 +0300

    feat: add DataBaseConnectionException class

--- src/main/java/edu/radyuk/foodblog/exception/DataBaseConnectionException.java
+++ src/main/java/edu/radyuk/foodblog/exception/DataBaseConnectionException.java
@@ -1,4 +1,18 @@
 package edu.radyuk.foodblog.exception;
 
-public class DataBaseConnectionException {
+public class DataBaseConnectionException extends Exception {
+    public DataBaseConnectionException() {
+    }
+
+    public DataBaseConnectionException(String message) {
+        super(message);
+    }
+
+    public DataBaseConnectionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DataBaseConnectionException(Throwable cause) {
+        super(cause);
+    }
 }
